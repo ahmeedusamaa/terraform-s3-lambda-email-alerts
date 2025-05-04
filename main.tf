f6b566253d2de408fbff8a0268dd0a78e4d705af
ahmed osama <ahmedosama21049@gmail.com>
2025-05-04 17:06:05 +0300

    edit main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "compute" {
-  source = "./modules/compute"
-  Application_ec2 = var.Application_ec2 
-  ami_id        = var.ami_id
-  public_subnets = module.network.public_subnet
-  private_subnets = module.network.private_subnet
-  app_security_groups = [module.network.app_sg_id]
-  Bastion_security_groups = [module.network.allow_ssh_sg_id]
-}
+# module "compute" {
+#   source = "./modules/compute"
+#   Application_ec2 = var.Application_ec2 
+#   ami_id        = var.ami_id
+#   public_subnets = module.network.public_subnet
+#   private_subnets = module.network.private_subnet
+#   app_security_groups = [module.network.app_sg_id]
+#   Bastion_security_groups = [module.network.allow_ssh_sg_id]
+# }
 
 module "network" {
   source = "./modules/network"
@@ -15,13 +15,13 @@ module "network" {
   region = var.region
 }
 
-module "storage" {
-  source = "./modules/storage"
-  redis_security_group = module.network.redis_sg_id
-  public_subnets = module.network.public_subnet
-  private_subnets = module.network.private_subnet
-  rds_instance = var.rds_instance
-  rds_security_group = module.network.rds_sg_id
-  redis_subnet_group = module.network.redis_sg_id
-}
+# module "storage" {
+#   source = "./modules/storage"
+#   redis_security_group = module.network.redis_sg_id
+#   public_subnets = module.network.public_subnet
+#   private_subnets = module.network.private_subnet
+#   rds_instance = var.rds_instance
+#   rds_security_group = module.network.rds_sg_id
+#   redis_subnet_group = module.network.redis_sg_id
+# }
 
--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket       = "bucket-terraform-state-0"
-    region       = "us-east-1"
-    key          = "terraform.tfstate"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket       = "bucket-terraform-state-0"
+#     region       = "us-east-1"
+#     key          = "terraform.tfstate"
+#     encrypt      = true
+#     use_lockfile = true
+#   }
+# }
 
 provider "aws" {
   region = var.region
